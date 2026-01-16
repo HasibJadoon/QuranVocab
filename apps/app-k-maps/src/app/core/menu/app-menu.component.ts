f4f58321aec868ee76b52f04860da040f30e942b
hasib Jadoon <hasibjadoon@gmail.com>
2026-01-16 19:37:39 +1100

    menu issue

--- apps/app-k-maps/src/app/core/menu/app-menu.component.ts
+++ apps/app-k-maps/src/app/core/menu/app-menu.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router';
+import { IonicModule, MenuController } from '@ionic/angular';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -10,4 +10,26 @@ import { RouterModule } from '@angular/router';
   standalone: true,
   imports: [CommonModule, IonicModule, RouterModule],
 })
-export class AppMenuComponent {}
+export class AppMenuComponent {
+  private openSections = new Set<string>(['arabic']);
+
+  constructor(private readonly router: Router, private readonly menu: MenuController) {}
+
+  async go(url: string) {
+    await this.menu.close();
+    await this.router.navigateByUrl(url);
+  }
+
+  toggle(section: string) {
+    if (this.openSections.has(section)) {
+      this.openSections.delete(section);
+    } else {
+      this.openSections.add(section);
+    }
+  }
+
+  isOpen(section: string) {
+    return this.openSections.has(section);
+  }
+}
+
